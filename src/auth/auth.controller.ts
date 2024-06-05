7a97eb6c46cc67402208ee29ba741949c8b25d7d
WestEastZ <tjehdwn10110@naver.com>
2024-06-05 16:06:07 +0900

    🐛 Fix Test [env, DB_connection]

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,6 @@ import { UserDto } from './user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private AuthService: AuthService) {}
-
   @Post('/signup')
   signup(@Body(ValidationPipe) UserDto: UserDto): Promise<void> {
     return this.AuthService.signUp(UserDto);
