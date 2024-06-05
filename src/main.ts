67f93ac222b05290ce1f168ccd9d189ea8936523
WestEastZ <tjehdwn10110@naver.com>
2024-06-05 18:00:39 +0900

    🐛 Fix [env, DB_connection]

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.SERVER_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  await app.listen(port);
+  Logger.log(`App ${port}`);
 }
 bootstrap();
