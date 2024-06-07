7e615b06104ef01647311db411d0fb02c6855835
WestEastZ <tjehdwn10110@naver.com>
2024-06-07 16:44:42 +0900

    ✨ Feat [Login, Jwt 토큰, async jwt env]

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,19 @@
-import { UserDto } from './user.dto';
-import { User } from './user.entity';
+import { UserDto } from './dto/user.dto';
+import { User } from './entity/user.entity';
 import { UserRepository } from './user.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(private UserRepository: UserRepository) {}
+
+  // 회원가입
   signUp(UserDto: UserDto): Promise<void> {
     return this.UserRepository.createUser(UserDto);
+  }
+
+  // 로그인
+  signin(UserDto: UserDto): Promise<{ accessToken: string }> {
+    return this.UserRepository.signin(UserDto);
   }
 }
