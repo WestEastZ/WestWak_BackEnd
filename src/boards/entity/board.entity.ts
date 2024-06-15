c6e21ea73f7e5d1a43aa4f9a5d4fffb615356e7f
WestEastZ <tjehdwn10110@naver.com>
2024-06-15 16:55:30 +0900

    ✨ Feat [Create Board]

---
+++ src/boards/entity/board.entity.ts
@@ -0,0 +1,26 @@
+import { User } from 'src/auth/entity/user.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { BoardStatus } from '../board.model';
+
+@Entity()
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+
+  @ManyToOne((type) => User, (user) => user.boards, {
+    eager: false,
+  })
+  user: User;
+}
