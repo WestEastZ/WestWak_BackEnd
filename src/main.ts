c6006dafcd4090cc48263003a38eb0e46cc4bf91
WestEastZ <tjehdwn10110@naver.com>
2024-06-25 14:22:49 +0900

    🐛 [Bug] CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const port = process.env.SERVER_PORT;
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3330'],
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+  });
   app.use(cookieParser());
   await app.listen(port);
   Logger.log(`App ${port}`);
