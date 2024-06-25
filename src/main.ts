77f782cce2fc0517d3fa13552a98378608708c0f
WestEastZ <tjehdwn10110@naver.com>
2024-06-25 14:24:24 +0900

    🐛 [Bug] CORS URL

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const port = process.env.SERVER_PORT;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3330'],
+    origin: ['http://localhost:3000'],
     credentials: true,
     exposedHeaders: ['Authorization'],
   });
