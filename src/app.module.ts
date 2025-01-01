0aaa3c276f7b07415c94e881ffbd4091c4db3064
WestEastZ <tjehdwn10110@naver.com>
2025-01-01 21:09:21 +0900

    🔧 [Config] Update TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { User } from './auth/entity/user.entity';
 import { BoardsModule } from './boards/boards.module';
-import { Board } from './boards/entity/board.entity';
 import { ScrapingModule } from './scraping/scraping.module';
 import { RecomendVideoModule } from './recomend-video/recomend-video.module';
 import { Top100Module } from './top100/top100.module';
-import { Top100 } from './top100/entity/top100.entity';
 import { InformationModule } from './information/information.module';
-import { Information } from './information/entity/infomation.entity';
 import { Awss3Module } from './awss3/awss3.module';
 import { BroadCastModule } from './broadcast/broad-cast.module';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
@@ -21,22 +18,7 @@ import { BroadCastModule } from './broadcast/broad-cast.module';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [
-        __dirname + '/../**/*.entity.{js.ts}',
-        User,
-        Board,
-        Top100,
-        Information,
-      ],
-      synchronize: false,
-    }),
+    TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
     BoardsModule,
     ScrapingModule,
